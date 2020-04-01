289406434f4d6761072d9fba2f73013f0a80f86a
Fernando Barbosa <fernando.barbosa@meta.com.br>
2020-04-01 10:15:30 -0300

    inclusao de loaders

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const host = process.env.NODE_ENV === 'local' ? 'http://localhost:3333' : 'https://feedev-be-the-hero-apid.herokuapp.com';
+const host = process.env.NODE_ENV === 'local' ? 'http://localhost:3333' : 'https://feedev-be-the-hero-api.herokuapp.com';
 
 const api = axios.create({
   baseURL: host,
